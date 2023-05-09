30846b566c3892b9111945bb9d1792ec3eb4e20f
Irina <lykin4ik@gmail.com>
2023-05-09 16:55:53 +0300

    .

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.forms import User
+from django.core.exceptions import ValidationError
 from myapp.models import Comment
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
@@ -9,6 +10,16 @@ class UserCreateForm(UserCreationForm):
     email = forms.EmailField(widget=forms.EmailInput)
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError('Email is already registered')
+        return email
 
 class CommentForm(forms.Form):
     # text = forms.CharField(max_length=500)
