c73f1573ec7501c9e0f33b44735c3c81ef4ad7ba
Irina <lykin4ik@gmail.com>
2023-05-15 22:07:56 +0300

    add signals

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-from myapp.models import Comment
+from myapp.models import Comment, PersonalPage
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 from django import forms
@@ -9,32 +9,24 @@ from django.contrib.auth import authenticate
 
 class UserCreateForm(UserCreationForm):
     email = forms.EmailField(help_text='A valid email address, please.', required=True)
-    # username = forms.CharField(label='Login', widget=forms.TextInput)
-    # email = forms.EmailField(widget=forms.EmailInput)
-    # password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-    # password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
 
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
 
-    # def save(self, commit=True):
-    #     user = super(UserCreateForm, self).save(commit=False)
-    #     user.email = self.cleaned_data['email']
-    #     if commit:
-    #         user.save()
-    #     return user
 
-    # def clean(self):
-    #     cleaned_data = super().clean()
-    #     username = cleaned_data.get('username')
-    #     password2 = cleaned_data.get('password2')
-    #     email = cleaned_data.get('email')
-    #     if username and password2:
-    #         self.user = authenticate(username=username, password2=password2, email=email)
-    #         if self.user is None:
-    #             raise forms.ValidationError('OOPS!!!')
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
 
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = PersonalPage
+        fields = ['user']
 
 class CommentForm(forms.Form):
     # text = forms.CharField(max_length=500)
--- myapp/views.py
+++ myapp/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth import get_user_model, authenticate, login, logout
 from myapp.models import Post, Comment
-from myapp.forms import CommentForm, UserCreateForm
+from myapp.forms import CommentForm, UserCreateForm, UserUpdateForm, ProfileUpdateForm
 
 
 
@@ -43,7 +43,26 @@ def registration(request):
 
 @login_required
 def profile(request):
-    return render(request, 'navigations/registration_done.html')
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, f'Профиль успешно обновлен.')
+            return redirect('profile')
+
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form
+    }
+
+    return render(request, 'navigations/registration_done.html', context)
 
 
 def logout_view(request):
